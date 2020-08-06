4598530682a5b710bce6c321a9415b9a30908d8b
Ayaz Araf <ayaz.araf@gmail.com>
2020-08-06 02:39:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,19 @@
 def reverse_each_word (sentence)
-  oa= sentence.split(' ')
+  oa= sentence.split(" ")
   ra=[]
   oa.each do |sentence|
     ra << oa.reverse
   end
   ra.join(" ")
   end
+def reverse_each_word(sentence)
+  ooa=sentence.split(" ")
+  rra=[]
+  ooa.collect do |sentence|
+    rra << ooa.reverse
+  end
+  rra.join(" ")
+end
+
 
 reverse_each_word("Hello there, and how are you?")
