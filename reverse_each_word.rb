9df080819022adbb137008048ec932171107a66f
Ayaz Araf <ayaz.araf@gmail.com>
2020-08-06 02:32:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,10 @@
 def reverse_each_word (string)
-  
+  oa= stirng.split(' ')
+  ra=[]
+  oa.each do |string|
+    ra = oa.reverse
+  end
+  ra.join(" ")  
+  end
+
+reverse_each_word("Hello how are you?")
