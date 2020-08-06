1578e8a8484da13c11e576966df56ed5d10d29f6
Ayaz Araf <ayaz.araf@gmail.com>
2020-08-06 02:43:33 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,25 @@
+def reverse_each_word_with_each(string)
+  oa= string.split(" ")
+  ra=[]
+  oa.each do |string|
+    ra << string.reverse
+  end
+  ra.join(" ")
+  end
+
+
+
+
+
+
+def reverse_each_word(string)
+  ooa=string.split(" ")
+  rra=[]
+  ooa.collect do |string|
+    rra << string.reverse
+  end
+  rra.join(" ")
+end
+
+reverse_each_word_with_each("Hello there, and how are you?")
+reverse_each_word("Hello there, and how are you?")
